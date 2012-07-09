0e00d1a428323518237ae978c1bbed9e72e8310b
Saju Madhavan <sajuptpm@gmail.com>
2012-07-09 12:03:31 +0530

    Update master

--- memento.py
+++ memento.py
@@ -79,3 +79,24 @@ if __name__ == '__main__':
       traceback.print_exc(0)
       pass
    print n
+
+"""
+<saju_m> Which is the Originator, Caretaker and Memento in this example  ?
+<lillis> saju_m: the memento is Memento, the caretacker I'd say is Transaction in this case (i.e. something which keeps check of all the memento object and their status
+<lillis> saju_m: originator is NumObj
+<lillis> i.e. the actual stateful object
+<lillis> saju_m: think of it like "some value", "a thing that holds the current and older values, as well as methods to go back" and "something that keeps track of all the things that are holding the values" basically
+<lillis> saju_m: Memento can be used to implement Undo operations for example
+<lillis> you have a stack of all the actions performed and these actions contain a method to revert themselves
+<lillis> then you just pop them off in run the .undo() or whatever
+<lillis> saju_m: try to focus on why to implement them instead ;)
+<lillis> that is, it's easier to understand a pattern if you understand its real world use
+<lillis> make a calculator in the console, with undo
+<lillis> where you can step back one operation at a time
+<lillis> it shouldnt be too hard and allows you to try out the memento pattern for example
+<saju_m> I this is the only way to implement memento design pattern in python ? https://github.com/gennad/Design-Patterns-in-Python/blob/master/memento.py
+<lillis> saju_m: no, that focuses on transactions (which is often very useful with the pattern)
+<lillis> but for example the undo/redo wouldn't be using transactions
+<lillis> saju_m: you dont have to focus on the exact code too much, the idea is the same in all programming languages
+<lillis> (or most)
+"""
